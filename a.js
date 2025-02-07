7c6f96accfc8996e5e5363e63eb7f171d6d641ca
Shubhamzambre-4100 <shubhamzambre4100@gmail.com>
2025-02-07 23:51:16 +0530

    shubham

---
+++ a.js
@@ -0,0 +1,12 @@
+
+function add() {
+    // Prompt user for input
+    let num1 = parseFloat(prompt("Enter the first number:"));
+    let num2 = parseFloat(prompt("Enter the second number:"));
+    
+    // Perform addition
+    let sum = num1 + num2;
+    
+    
+    console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
